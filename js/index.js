de3b43c55a7c5dd914d0370af38be50bc1eee6cb
maddymastering <maduvce26@gmail.com>
2025-06-06 10:50:43 +0530

    Add contact page

---
+++ js/index.js
@@ -0,0 +1,15 @@
+function openSidebar() {
+    document.querySelector('body').classList.add('no-overflow');
+    document.getElementById('aside').classList.add('open');
+    document.querySelector('.overlay').classList.add('show');
+}
+
+function closeSidebar() {
+    document.querySelector('body').classList.remove('no-overflow');
+    document.getElementById('aside').classList.remove('open');
+    document.querySelector('.overlay').classList.remove('show');
+}
+
+function gotoContact() {
+    window.location.href = '/contact.html';
+}
